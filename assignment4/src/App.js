afbab3357a281e216e19147a9ca345ca17c0e401
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-22 08:04:16 +0000

    App file edited

--- assignment4/src/App.js
+++ assignment4/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Details from './components/Details';
 import { animals } from './data/AnimalsDb';
+import '../src/assets/css/animal.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Animal Matching Game</h1>
+      
       <Details animals={animals}/>
     </div>
   );

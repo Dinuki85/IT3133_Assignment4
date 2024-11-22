5a52196cffa1dcccea8044c55804652a92387118
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-22 05:36:15 +0000

    start to creating a tables

---
+++ assignment4/src/components/Details.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from "react";
+
+export default function Details(props){
+    return(
+        <div>
+            <div>
+                <table border="1">
+                    <thead>
+                        <tr>
+                            <td>Animal Name</td>
+                            <td>Action</td>
+                            
+                        </tr>
+                    </thead>
+
+                </table>
+            </div>
+        </div>
+    )
+}

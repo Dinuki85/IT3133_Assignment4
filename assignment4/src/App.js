dcb26521755cb3e1a0e845c9ac43e3176f2d34d5
Dinuki85 <139613301+Dinuki85@users.noreply.github.com>
2024-11-22 05:36:42 +0000

    Added main file and animal details connected.

--- assignment4/src/App.js
+++ assignment4/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Details from './components/Details';
+import { animals } from './data/AnimalsDb';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animal Matching Game</h1>
+      <Details animals={animals}/>
     </div>
   );
 }
+
 
 export default App;
